4acca03b5b1240a1e58e9b35b1399bdef593ec19
isacc <isacc@LAPTOP-9NF5A4BN>
2024-01-26 14:27:00 +0100

    risoluzione primo punto.

--- src/gestionefile/GestioneFile.java
+++ src/gestionefile/GestioneFile.java
@@ -5,6 +5,11 @@ package gestionefile;
  * @author RazvanDrosu
  * @version 23/01/24
  */
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
  import java.util.Scanner;
 
 public class GestioneFile {
@@ -13,6 +18,8 @@ public class GestioneFile {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        String sourceFilePath = "output.csv";
+        String destinationFilePath = "copia.csv";
         
         //1)LETTURA
         Lettore lettore = new Lettore("user.json");
@@ -27,9 +34,32 @@ public class GestioneFile {
         // Chiedi all'utente di inserire la password
         System.out.print("Inserisci la password: ");
         String password = scanner.nextLine();
-
+        
         // Chiudi lo scanner
         scanner.close();
+        try {
+            // Apri il file sorgente in lettura usando BufferedReader
+            BufferedReader reader = new BufferedReader(new FileReader(sourceFilePath));
+
+            // Apri il file di destinazione in scrittura usando BufferedWriter
+            BufferedWriter writer = new BufferedWriter(new FileWriter(destinationFilePath));
+
+            // Leggi ogni riga dal file sorgente e scrivila nel file di destinazione
+            String line;
+            while ((line = reader.readLine()) != null) {
+                writer.write(line);
+                writer.newLine(); // Aggiungi una nuova linea nel file di destinazione dopo ogni riga
+            }
+
+            // Chiudi le risorse
+            reader.close();
+            writer.close();
+
+            System.out.println("Il file è stato copiato con successo.");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         
         //3) SCRITTURA
         Scrittore scrittore = new Scrittore("output.csv", username, password);
